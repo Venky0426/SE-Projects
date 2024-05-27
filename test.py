87423730d71777cff18abd96a5bd1e2dfc6f6a87
Venky0426 <venkeyhm562@gmail.com>
2024-05-27 17:27:08 +0530

    Changes

--- test.py
+++ test.py
@@ -3,8 +3,12 @@ int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
 <<<<<<< HEAD
+<<<<<<< HEAD
    return 12;
 =======
    return 11;
 >>>>>>> 772e5e9f4c92592460496f47100c7db369e28e02
+=======
+   return 200 ;
+>>>>>>> 8bce45b0c53397d7015e3f6aaffe9338d56acfaf
 }
